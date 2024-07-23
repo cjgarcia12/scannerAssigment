633fd3ca282c66ad460e9328e14bffe2dd78b89f
cjgarcia12 <christian.jgarcia03@gmail.com>
2024-07-23 11:17:40 -0400

    init commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What's your name?");
+        String name = scanner.nextLine();
+        System.out.println("Hi " + name);
+    }
 }
